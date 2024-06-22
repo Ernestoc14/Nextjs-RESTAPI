ba9260f5a0680d3886995c09b46ab8e856e7cde8
Ernestoc14 <descreed34@hotmail.com>
2024-06-22 09:07:25 -0500

    Fix: Type Error in MongoDB URI and Creating users in database

--- src/app/api/(auth)/users/route.js
+++ src/app/api/(auth)/users/route.js
@@ -1,5 +1,17 @@
+import connectDB from "/lib/db";
+import User from "/lib/modals/user";
 import { NextResponse } from "next/server";
 
-export const GET = () => {
-  return new NextResponse("This is my first api.");
+export const GET = async () => {
+  try {
+    await connectDB();
+    const users = await User.find();
+    return new NextResponse(JSON.stringify(users), { status: 200 });
+  } catch (error) {
+    return new NextResponse("Error in fetching users " + error.message, {
+      status: 500,
+    });
+  }
 };
+
+
