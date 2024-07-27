48433051cc06482df92126ff7f177eeb5b3f951f
Ernestoc14 <descreed34@hotmail.com>
2024-07-26 23:22:51 -0500

    Feat: Create a Category Schema into the Modals

---
+++ src/app/api/(dashboard)/categories/route.js
@@ -0,0 +1,12 @@
+import connectDB from "../../../../../lib/db";
+import User from "../../../../../lib/modals/user";
+import Category from "../../../../../lib/modals/category";
+import { NextResponse } from "next/server";
+import { Types } from "mongoose";
+
+export const GET = async (req) => {
+  try {
+  } catch (error) {
+    
+  }
+};
